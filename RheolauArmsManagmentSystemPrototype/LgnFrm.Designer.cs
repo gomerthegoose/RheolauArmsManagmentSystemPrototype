624e5af40031a57693898e9da15e5f79a92af2a3
gomerthegoose <gomerthegoose@gmail.com>
2022-11-08 21:32:07 +0000

    initial commit

---
+++ RheolauArmsManagmentSystemPrototype/LgnFrm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace RheolauArmsManagmentSystemPrototype
+{
+    partial class LgnFrm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lgnPnl = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lgnPnl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lgnPnl
+            // 
+            this.lgnPnl.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lgnPnl.Controls.Add(this.label2);
+            this.lgnPnl.Controls.Add(this.label1);
+            this.lgnPnl.Controls.Add(this.textBox2);
+            this.lgnPnl.Controls.Add(this.textBox1);
+            this.lgnPnl.Controls.Add(this.button1);
+            this.lgnPnl.Location = new System.Drawing.Point(84, 46);
+            this.lgnPnl.Name = "lgnPnl";
+            this.lgnPnl.Size = new System.Drawing.Size(229, 141);
+            this.lgnPnl.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Forgot Password ?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(3, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.PlaceholderText = "Password";
+            this.textBox2.Size = new System.Drawing.Size(219, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Username";
+            this.textBox1.Size = new System.Drawing.Size(219, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "- Login -------------";
+            // 
+            // LgnFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 286);
+            this.Controls.Add(this.lgnPnl);
+            this.Name = "LgnFrm";
+            this.Text = "Login";
+            this.Resize += new System.EventHandler(this.LgnFrm_Resize);
+            this.lgnPnl.ResumeLayout(false);
+            this.lgnPnl.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel lgnPnl;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label2;
+    }
+}
---
+++ RheolauArmsManagmentSystemPrototype/LgnFrm.cs
@@ -0,0 +1,25 @@
+namespace RheolauArmsManagmentSystemPrototype
+{
+    public partial class LgnFrm : Form
+    {
+        public LgnFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LgnFrm_Resize(object sender, EventArgs e)
+        {
+            lgnPnl.Location = new Point (LgnF lgnPnl.Size.Width/2 )
+        }
+    }
+}
