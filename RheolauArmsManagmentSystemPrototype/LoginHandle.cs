8cf91e7a9ea7162affa1ce6c1060f12c6bdf51a2
gomerthegoose <gomerthegoose@gmail.com>
2022-11-10 19:34:32 +0000

    ewmother

--- RheolauArmsManagmentSystemPrototype/LoginHandle.cs
+++ RheolauArmsManagmentSystemPrototype/LoginHandle.cs
@@ -3,17 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace RheolauArmsManagmentSystemPrototype
 {
     internal class LoginHandle
     {
 
-        public bool Login(string Username , string Password)
+        public struct UsrInfo //used to store usr info
+        {
+            int ID;
+            string Username;
+            string password;
+            int accessLevel;
+            string RawData;
+        }
+
+        public bool Login(string Username , string Password) //handle login returns true if successfull login
         {
             LoginSettings loginSettings = new LoginSettings(); //create nenw instance of login settings
-            
+            getUsrData(loginSettings.UsrDetailsFile);
             return false;
+        }
+
+        private UsrInfo getUsrData(string fileLocation)
+        {
+            UsrInfo usrInfo = new UsrInfo();
+            Cryptography cryptography = new Cryptography();
+
+            using (StreamReader Sr = new StreamReader(fileLocation))
+            {
+                usrInfo. cryptography.decryptStr(Sr.ReadToEnd());
+            }
+            
+
+            
+            return usrInfo;
         }
     }
 }
---
+++ RheolauArmsManagmentSystemPrototype/cryptography.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RheolauArmsManagmentSystemPrototype
+{
+    internal class Cryptography
+    {
+
+        public string encryptStr(string input)
+        {
+            return "";
+        }
+
+        public string decryptStr(string input)
+        {
+            return "";
+        }
+
+    }
+}
